4b6c6ae25d8e60add6efa2dbc0b659ebdb0638b0
getcoinbytes@gmail.com <getcoinbytes@gmail.com>
2023-12-21 14:14:35 +0000

    Saved recipe: SAMPLEPROJECT.compute_stg_orders

--- recipes/compute_stg_orders.sql
+++ recipes/compute_stg_orders.sql
@@ -1,8 +1,7 @@
-     select
-            id as order_id,
-            user_id as customer_id,
-            order_date,
-            status
-        from
-            "PC_DATAIKU_DB"."JAFFLE_SHOP"."ORDERS"
-   
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from
+    "PC_DATAIKU_DB"."JAFFLE_SHOP"."ORDERS"
