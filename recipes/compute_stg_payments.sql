54b362d1b761ecd44ebbc9e975db07162d86a901
getcoinbytes@gmail.com <getcoinbytes@gmail.com>
2023-12-21 14:19:31 +0000

    Saved recipe: SAMPLEPROJECT.compute_stg_payments

---
+++ recipes/compute_stg_payments.sql
@@ -0,0 +1,16 @@
+ 
+  create or replace   view  stg_payments
+  
+   as (
+    select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    
+    (amount / 100)::numeric(16, 2)
+  as amount,
+    created as created_at
+from raw.stripe.payment
+  );
+
