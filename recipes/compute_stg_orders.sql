27fda1af016057a5c01ab401197f6ef3d9692212
getcoinbytes@gmail.com <getcoinbytes@gmail.com>
2023-12-21 13:46:08 +0000

    Saved recipe: SAMPLEPROJECT.compute_stg_orders

--- recipes/compute_stg_orders.sql
+++ recipes/compute_stg_orders.sql
@@ -1,19 +1,16 @@
- 
-  create or replace   view stg_orders
-  
-   as (
+create
+or replace view stg_orders as (
     with orde as (
-
+        select
+            id as order_id,
+            user_id as customer_id,
+            order_date,
+            status
+        from
+            ORDERS
+    )
     select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from orders
-
-)
-
-select * from orde
-  );
-
+        *
+    from
+        orde
+);
